d5ffca5b3289b991458ac8edc8a01177b50badb8
shawntsai0312 <shawntsai0312@gmail.com>
2024-09-07 14:16:46 +0800

    “

--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,34 +1,11 @@
-'use client'
-import Choicebar from "./choicebar";
-import { useEffect, useState } from 'react';
-import Deck from './deck';
-import users from '../../../public/resource/homeData/users.json'
+import * as React from 'react';
 
 const Home = () => {
-    const [currCardIndex, setCurrCardIndex] = useState<number>(users.length - 1);
-    const [choiceRate, setChoiceRate] = useState<number>(0);
-    const [choices, setChoices] = useState<Array<'like' | 'dislike' | 'none'>>(new Array(users.length).fill('none'));
-
-    useEffect(() => {
-        if (choiceRate >= 5.5 || choiceRate <= -5.5) setChoiceRate(0)
-    }, [choiceRate])
-
-    useEffect(() => {
-        // console.log(currCardIndex)
-    }, [currCardIndex])
-
     return (
-        <div className="justify-center items-center h-full w-full overflow-y relative">
-            <Deck setChoiceRate={setChoiceRate}
-                choices={choices} setChoices={setChoices}
-                currCardIndex={currCardIndex} setCurrCardIndex={setCurrCardIndex}
-            />
-            <div className="w-full h-[44px] fixed flex justify-center bottom-[90px] z-30">
-                <Choicebar choiceRate={choiceRate} setChoiceRate={setChoiceRate}
-                    currCardIndex={currCardIndex} setCurrCardIndex={setCurrCardIndex}
-                    choices={choices} setChoices={setChoices}
-                />
-            </div>
+        <div className="w-full h-[17%] flex row justify-around items-center">
+            <div>hello 1</div>
+            <div>hello 2</div>
+            <div>hello 3</div>
         </div>
     )
 }

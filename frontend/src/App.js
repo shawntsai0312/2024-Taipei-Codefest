4b5ba59f00606444ae6f457398c6726f343bff1f
shawntsai0312 <shawntsai0312@gmail.com>
2024-09-07 12:30:46 +0800

    router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './containers/Navbar';
+import Home from './containers/Home';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="w-full h-8 fixed flex justify-center bottom-0 bg-white">
+        <Navbar />
+      </div>
+      <div className="w-full h-[calc(100%-48px)] fixed overflow-y top-[48px] z-10">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {/* <Route path="/chat" element={<Chat />} />
+          <Route path="/profile" element={<Profile />} /> */}
+        </Routes>
+      </div>
+    </Router>
+
   );
 }
 

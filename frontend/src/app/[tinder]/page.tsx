5af20e79f4489f2cade256ab9ca3cb3252c4be6f
shawntsai0312 <shawntsai0312@gmail.com>
2024-09-07 19:25:05 +0800

    tinder complete

---
+++ frontend/src/app/[tinder]/page.tsx
@@ -0,0 +1,82 @@
+'use client'
+import Choicebar from "./choicebar";
+import dynamic from 'next/dynamic';
+import { useEffect, useState } from 'react';
+import { usePathname } from "next/navigation";
+import Deck from './deck';
+
+interface Place {
+    name: string;
+    description: string;
+    coordinates: {
+        latitude: number;
+        longitude: number;
+    };
+    id: number;
+    imgUrl: string[];
+    price: string;
+    time: string;
+    rating: number;
+}
+
+const Tinder = () => {
+    const pathname = usePathname();
+    const [jsonData, setJsonData] = useState<Place[] | null>(null);
+
+    useEffect(() => {
+        console.log(pathname);
+        const loadJsonData = async () => {
+            try {
+                // Dynamically import JSON file based on condition
+                const loadedJsonData = await import(`../../../public/data${pathname}.json`);
+                setJsonData(loadedJsonData.default);
+            } catch (error) {
+                console.error('Error loading JSON data:', error);
+            }
+        };
+
+        loadJsonData();
+    }, [pathname])
+
+    const [currCardIndex, setCurrCardIndex] = useState<number>(jsonData ? jsonData.length - 1 : 0);
+    const [choiceRate, setChoiceRate] = useState<number>(0);
+    const [choices, setChoices] = useState<Array<'like' | 'dislike' | 'none'>>(jsonData ? new Array(jsonData.length).fill('none') : []);
+
+    useEffect(() => {
+        setCurrCardIndex(jsonData ? jsonData.length - 1 : 0);
+    }, [jsonData])
+
+    useEffect(() => {
+        if (choiceRate >= 5.5 || choiceRate <= -5.5) setChoiceRate(0)
+    }, [choiceRate])
+
+    useEffect(() => {
+        console.log(currCardIndex)
+    }, [currCardIndex])
+
+    useEffect(() => {
+        console.log(choices)
+    }, [choices])
+
+    return (
+        <div className="w-full h-full justify-center items-center overflow-y relative">
+            <Deck setChoiceRate={setChoiceRate}
+                choices={choices} setChoices={setChoices}
+                currCardIndex={currCardIndex} setCurrCardIndex={setCurrCardIndex}
+                jsonData={jsonData}
+            />
+            <div className="w-full h-[44px] fixed flex justify-center bottom-[90px] z-30">
+                {
+                    currCardIndex >= 0 ?
+                        <Choicebar choiceRate={choiceRate} setChoiceRate={setChoiceRate}
+                            currCardIndex={currCardIndex} setCurrCardIndex={setCurrCardIndex}
+                            choices={choices} setChoices={setChoices}
+                        /> : <></>
+                }
+
+            </div>
+        </div>
+    )
+}
+
+export default Tinder;
--- frontend/src/app/home/page.tsx
+++ frontend/src/app/home/page.tsx
@@ -1,11 +1,39 @@
+'use client'
 import * as React from 'react';
+import { useRouter, useParams } from 'next/navigation';
+const tinderType = [
+    {
+        "type": "art"
+    },
+    {
+        "type": "child"
+    },
+    {
+        "type": "couple"
+    }
+]
 
 const Home = () => {
+
+    const router = useRouter();
+    
     return (
         <div className="w-full h-[17%] flex row justify-around items-center">
-            <div>hello 1</div>
-            <div>hello 2</div>
-            <div>hello 3</div>
+            {
+                tinderType.map(each => {
+                    return (
+                        <div
+                            className='w-[30%] h-full flex justify-center items-center'
+                            onClick={() => router.push(`/${each.type}`)}
+                            key={each.type}
+                        >
+                            {
+                                each.type
+                            }
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
